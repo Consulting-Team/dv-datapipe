73214ea311015cf4269261fe26c91ae8d7ec0a2f
Jeong Min <kjfootman@hanwha.com>
2026-03-16 08:57:07 +0000

    임시 테이블에서 생성된 테이블로 데이터복사 기능 추가

--- iceberg_test.py
+++ iceberg_test.py
@@ -5,6 +5,7 @@ from jmlogger import logger
 from connection import ImpalaConnection, abfs
 from utils.metadata import read_metadata, MetaData, get_db_name
 from functools import reduce
+from pathlib import Path
 import polars as pl
 
 # 테이블이 생성될 DB
@@ -13,7 +14,7 @@ DB_NAME = "tmp"
 TABLE_NAME = f"{config.hull}_iceberg_test"
 
 
-def get_storage_location() -> str:
+def get_storage_location(hull=None) -> str:
     # 클라우드 테이블 저장 위치 반환
     container_name = config.hs4v1_abfs_strg_cont
     account_name = config.hs4v1_abfs_strg_acc
@@ -39,8 +40,95 @@ def clear_table():
         abfs.rm(location, recursive=True)
 
 
-def create_iceberg_table(metadata_list: dict[str, MetaData]):
+# def insert_data(
+#     col_names: list[str], metadata_dict: dict[str, MetaData], start: str, end: str
+# ):
+#     conn = ImpalaConnection().conn
+
+#     # source table에서 데이터 쿼리
+#     df = query_data(metadata_dict=metadata_dict, start=start, end=end)
+#     df = df.with_columns(pl.from_epoch("ds_timestamp", time_unit="ms"))
+#     data = df.to_dicts()
+
+#     # print(df)
+#     col_str = ",\n\t".join(col_names)
+#     val_str = ",\n\t".join([f"%({col})s" for col in col_names])
+#     sql = f"INSERT INTO {DB_NAME}.{TABLE_NAME} ({col_str}) VALUES ({val_str})"
+
+#     logger.debug(sql)
+
+#     cursor = conn.cursor()
+#     cursor.executemany(sql, data)
+
+#     return
+
+
+def insert_data2(
+    col_names: list[str], metadata_dict: dict[str, MetaData], start: str, end: str
+):
+    conn = ImpalaConnection().conn
+    cursor = conn.cursor()
+    cursor.execute("SET PARQUET_FALLBACK_SCHEMA_RESOLUTION=name")
+
+    base = get_storage_location().replace(f"/{config.hull}/", "")
+    tmp_dir = f"{str(base)}/tmpdata"
+    tmp_parquet_path = f"{tmp_dir}/{start}_{end}.parquet"
+    tmp_tbl_name = f"{config.hull}_{start}_{end}"
+
+    # source table에서 데이터 쿼리
+    df = query_data(metadata_dict=metadata_dict, start=start, end=end)
+    df = df.with_columns(pl.from_epoch("ds_timestamp", time_unit="ms"))
+
+    # 임시 테이블 생성
+    ## parquet 저장 경로 생성
+    if not abfs.exists(tmp_dir):
+        abfs.makedirs(tmp_dir, exist_ok=True)
+        logger.debug(f"Temporary Dataframe has been copied to '{tmp_dir}'.")
+
+    ## Azure에 데이터 프레임 저장
+    df.write_parquet(
+        tmp_parquet_path,
+        storage_options={
+            "account_name": config.hs4v1_abfs_strg_acc,
+            "account_key": config.hs4v1_abfs_strg_key,
+        },
+    )
+
+    ## 임시 테이블 생성 및 파티션 연결
+    ### 동일한 테이블 이름이 있으면 삭제
+    cursor.execute(f"DROP TABLE IF EXISTS tmp.{tmp_tbl_name}")
+    ### sql 문작성
+    sql = f"""
+        CREATE EXTERNAL TABLE tmp.{tmp_tbl_name} (
+            {',\n\t'.join(col_names)}
+        )
+        STORED AS PARQUET
+        LOCATION '{tmp_dir}'
+    """
+    cursor.execute(sql)
+
+    # 임시 테이블에서 데이터 복사
+    col_names_str = ",\n\t".join(
+        [str_value.split()[0].strip() for str_value in col_names]
+    )
+    sql = f"""INSERT INTO {DB_NAME}.{TABLE_NAME} ({col_names_str})
+    SELECT {col_names_str} FROM tmp.{tmp_tbl_name}
+    """
+    logger.debug(sql)
+    cursor.execute(sql)
+
+    # 임시 테이블 및 데이터 파일 삭제
+    # ## 임시 데이터 파일 삭제
+    if abfs.exists(tmp_dir):
+        abfs.rm(tmp_dir, recursive=True)
+
+    ## 임시 테이블 삭제
+    cursor.execute(f"DROP TABLE IF EXISTS tmp.{tmp_tbl_name}")
+
+
+def create_iceberg_table(metadata_dict: dict[str, MetaData]) -> list[str]:
     # Iceberg 테이블 생성
+    # 생성된 테이블의 열을 배열로 반환
     table_name = f"{DB_NAME}.{TABLE_NAME}"
     connection = ImpalaConnection()
 
@@ -48,17 +136,21 @@ def create_iceberg_table(metadata_list: dict[str, MetaData]):
     location = get_storage_location()
 
     # column 정의
-    cols = [
-        f"{m.col_name}    {m.data_type}"
-        for data in metadata_list.values()
-        for m in data
-    ]
-    cols = ",\n".join(cols)
+    cols = ["ds_timestamp    TIMESTAMP"]
+    cols.extend(
+        [
+            f"{m.col_name}    {m.data_type}"
+            for metadata_list in metadata_dict.values()
+            for m in metadata_list
+        ]
+    )
+    cols.extend([f"id_{table_name}    BIGINT" for table_name in metadata_dict.keys()])
+    cols_str = ",\n".join(cols)
 
     # iceberg 테이블 생성 sql
     sql = f"""
         CREATE TABLE IF NOT EXISTS {table_name} (
-            {cols}
+            {cols_str}
         )
         PARTITIONED BY SPEC (MONTHS(ds_timestamp))
         STORED AS ICEBERG
@@ -81,132 +173,10 @@ def create_iceberg_table(metadata_list: dict[str, MetaData]):
     for row in rs:
         logger.debug(row[0])
 
-
-def copy_data_join(metadata_list: dict[str, MetaData]):
-    # id(timestamp)로 joint하여 Iceberg 테이블에 데이터 복사
-    connection = ImpalaConnection()
-
-    target_table = f"{DB_NAME}.{TABLE_NAME}"
-    join_clauses = []
-    select_columns = []
-
-    for i, (table_name, metadata) in enumerate(metadata_list.items()):
-        alias = f"s{i}"
-        tags = list()
-        # 각 소스 테이블에서 가져올 컬럼들에 CAST 적용
-        for m in metadata:
-            # if "src.ds_timestamp" not in tags:
-            tags.append(f"src.{m.tag}")
-            select_columns.append(f"CAST({alias}.{m.tag} AS {m.data_type})")
-
-        tags_str = ",\n".join(list(set(tags)))
-
-        # ±10초(10000ms) 내에서 가장 가까운 행 1개만 매칭 (그 외에는 자동으로 NULL)
-        join_sql = f"""
-            LEFT JOIN (
-                SELECT *, ROW_NUMBER() OVER (PARTITION BY ref_id ORDER BY diff ASC) AS rn
-                FROM (
-                    SELECT
-                        {tags_str},
-                        t_ref.id AS ref_id,
-                        ABS(t_ref.id - CAST(src.ds_timestamp AS DOUBLE)) AS diff
-                    FROM {target_table} t_ref
-                    JOIN {"hlng"}.{table_name} src
-                    ON CAST(src.ds_timestamp AS DOUBLE) BETWEEN (t_ref.id - 10000) AND (t_ref.id + 10000)
-                ) AS tmp_{i}
-            ) AS {alias} ON t.id = {alias}.ref_id AND {alias}.rn = 1
-        """
-        join_clauses.append(join_sql)
-
-    sql = f"""
-        INSERT OVERWRITE {target_table}
-        SELECT DISTINCT {', '.join(select_columns)}
-        FROM {target_table} t
-        {' '.join(join_clauses)}
-    """
-    logger.debug(sql)
-
-    cursor = connection.conn.cursor()
-    cursor.execute(sql)
+    return cols
 
 
-def copy_data(metadata_list: list[MetaData], start: str, end: str):
-    # 특정 테이블에 있는 데이터를 새로 생성한 Iceberg 테이블로 복사
-    connection = ImpalaConnection()
-
-    db_name = list(set([m.db_name for m in metadata_list]))
-    table_name = list(set([m.table_name for m in metadata_list]))
-
-    if not db_name:
-        err_msg = "Not available metadata - db_name is empty."
-        logger.error(err_msg)
-        raise ValueError(err_msg)
-
-    if len(db_name) > 1:
-        err_msg = "Not available metadata - db_name is not unique."
-        logger.error(err_msg)
-        raise ValueError(err_msg)
-
-    if not table_name:
-        err_msg = "Not available metadata - table_name is empty."
-        logger.error(err_msg)
-        raise ValueError(err_msg)
-
-    if len(table_name) != 1:
-        err_msg = "Not available metadata - table_name is not unique."
-        logger.error(err_msg)
-        raise ValueError(err_msg)
-
-    db_name = db_name[0]
-    table_name = table_name[0]
-
-    # 복사할 열 세팅
-    cols = list()
-    tags = list()
-    for m in metadata_list:
-        cols.append(m.col_name)
-
-        # timestamp 형 변환
-        if "timestamp" in m.col_name.lower():
-            tags.append(
-                f"FROM_UNIXTIME(CAST(CAST({m.tag}    AS    BIGINT) / 1000 AS BIGINT)) AS ds_timestamp"
-            )
-            continue
-
-        # id 형 변환
-        if "id" in m.col_name.lower():
-            tags.append(f"CAST(CAST(ds_timestamp    AS    DOUBLE) AS BIGINT) AS id")
-            continue
-
-        # 메타데이터 정의대로 형 변환
-        tags.append(f"CAST({m.tag}    AS    {m.data_type})")
-
-    cols = ",\n".join(cols)
-    tags = ",\n".join(tags)
-
-    # ias_no1 데이터 복사
-    sql = f"""
-        INSERT INTO {DB_NAME}.{TABLE_NAME} (
-            {cols}
-        )
-        SELECT
-            {tags}
-        FROM {db_name}.{table_name}
-        WHERE ds_date BETWEEN '{start}' AND '{end}'
-        ORDER BY ds_timestamp
-       -- limit 10;
-    """
-
-    # print(sql)
-    # logger.debug(sql)
-
-    cursor = connection.conn.cursor()
-    cursor.execute(sql)
-
-    logger.info(f"Data from the '{table_name}' table has been successfully copied.")
-
-
-# def copy_data_test(metadata_list: list[MetaData], start: str, end: str) -> pl.DataFrame:
+# def copy_data(metadata_list: list[MetaData], start: str, end: str):
 #     # 특정 테이블에 있는 데이터를 새로 생성한 Iceberg 테이블로 복사
 #     connection = ImpalaConnection()
 
@@ -228,7 +198,7 @@ def copy_data(metadata_list: list[MetaData], start: str, end: str):
 #         logger.error(err_msg)
 #         raise ValueError(err_msg)
 
-#     if len(table_name) > 1:
+#     if len(table_name) != 1:
 #         err_msg = "Not available metadata - table_name is not unique."
 #         logger.error(err_msg)
 #         raise ValueError(err_msg)
@@ -236,63 +206,54 @@ def copy_data(metadata_list: list[MetaData], start: str, end: str):
 #     db_name = db_name[0]
 #     table_name = table_name[0]
 
-#     # ---------------------------------------------------------------------------------------------------
-#     col_map = dict()
+#     # 복사할 열 세팅
+#     cols = list()
 #     tags = list()
-
-#     tags.append(f"CAST(CAST(ds_timestamp    AS    DOUBLE) AS BIGINT) AS ds_timestamp")
-
 #     for m in metadata_list:
+#         cols.append(m.col_name)
+
 #         # timestamp 형 변환
-#         if "ds_timestamp" == m.col_name.lower():
-#             # tags.append(
-#             #     f"CAST(CAST(ds_timestamp    AS    DOUBLE) AS BIGINT) AS ds_timestamp"
-#             # )
+#         if "timestamp" in m.col_name.lower():
+#             tags.append(
+#                 f"FROM_UNIXTIME(CAST(CAST({m.tag}    AS    BIGINT) / 1000 AS BIGINT)) AS ds_timestamp"
+#             )
 #             continue
 
 #         # id 형 변환
-#         if "id" == m.col_name.lower():
+#         if "id" in m.col_name.lower():
 #             tags.append(f"CAST(CAST(ds_timestamp    AS    DOUBLE) AS BIGINT) AS id")
 #             continue
 
 #         # 메타데이터 정의대로 형 변환
-#         tags.append(f"CAST({m.tag}    AS    {m.data_type})    AS    {m.tag}")
-#         col_map[m.tag] = m.col_name
+#         tags.append(f"CAST({m.tag}    AS    {m.data_type})")
 
-#     tags = ",\n\t".join(tags)
+#     cols = ",\n".join(cols)
+#     tags = ",\n".join(tags)
 
-#     # ? 데이터프레임으로 로드 후 리샘플링해서 적재
+#     # ias_no1 데이터 복사
 #     sql = f"""
-#         SELECT
-#         {tags}
-#         FROM
-#         {db_name}.{table_name}
-#         WHERE ds_date BETWEEN '{start}' AND '{end}';
-#     """
-#     # print(sql)
-#     df = connection.query_polars(sql=sql)
-#     df = df.sort("ds_timestamp")
-#     # tag 매핑
-#     df = df.rename(col_map)
-
-#     synced_df = (
-#         df.with_columns(
-#             pl.from_epoch(pl.col("ds_timestamp"), time_unit="ms")
-#             .dt.round("15s")
-#             .alias("time_sync")
+#         INSERT INTO {DB_NAME}.{TABLE_NAME} (
+#             {cols}
 #         )
-#         .group_by("time_sync")
-#         .agg(pl.all().first())
-#         .sort("time_sync")
-#     )
+#         SELECT
+#             {tags}
+#         FROM {db_name}.{table_name}
+#         WHERE ds_date BETWEEN '{start}' AND '{end}'
+#         ORDER BY ds_timestamp
+#        -- limit 10;
+#     """
 
-#     # print(synced_df)
-#     return synced_df
-#     # ---------------------------------------------------------------------------------------------------
+#     # print(sql)
+#     # logger.debug(sql)
+
+#     cursor = connection.conn.cursor()
+#     cursor.execute(sql)
+
+#     logger.info(f"Data from the '{table_name}' table has been successfully copied.")
 
 
 def query_data(
-    metadata_dict: dict[str, list[MetaData]], departure: str, arrival: str
+    metadata_dict: dict[str, list[MetaData]], start: str, end: str
 ) -> pl.DataFrame:
     # 기존 테이블에서 데이터 쿼리
 
@@ -329,11 +290,20 @@ def query_data(
             {',\n\t'.join(tags)}
             FROM
             {db_name}.{table_name}
-            WHERE ds_date BETWEEN '{departure}' AND '{arrival}';
+            WHERE ds_date BETWEEN '{start}' AND '{end}';
         """
+
+        # logger.debug(sql)
 
         # 데이터 쿼리
         df = conn.query_polars(sql=sql)
+        # 32비트 변수로 변경
+        df = df.with_columns(
+            [
+                pl.col(pl.Int64).exclude("ds_timestamp").cast(pl.Int32),
+                pl.col(pl.Float32).cast(pl.Float64),
+            ]
+        )
 
         if df.is_empty():
             logger.warning(f"The query result set for table '{table_name}' is empty.")
@@ -346,7 +316,7 @@ def query_data(
             df.with_columns(
                 pl.from_epoch(pl.col(f"id_{table_name}"), time_unit="ms")
                 .dt.round("15s")
-                .alias("time_sync")
+                .alias("time_sync"),
             )
             .group_by("time_sync")
             .agg(pl.all().first())
@@ -358,7 +328,15 @@ def query_data(
         combined_df = reduce(
             lambda left, right: left.join(right, on="time_sync", how="left"),
             list(df_set.values()),
+        ).with_columns(
+            pl.col("time_sync").cast(pl.Int64).alias("ds_timestamp"),
         )
+
+        # df = df.with_columns([
+        #     pl.col(pl.Int64).cast(pl.Int32),
+        #     pl.col(pl.Int32).cast(pl.Int32), # 이미 32인 것도 확인
+        #     pl.col(pl.Float32).cast(pl.Float64)
+        # ])
 
     return combined_df
 
@@ -367,43 +345,14 @@ def main():
     # 메타데이터 읽기
     metadata_list = read_metadata(f"resources/csv/{config.hull}_metadata.csv")
 
-    # # 기존 테이블 및 데이터 삭제
-    # clear_table()
+    # 기존 테이블 및 데이터 삭제
+    clear_table()
 
     # Iceberg 테이블 생성
-    # create_iceberg_table(metadata_list)
+    col_names = create_iceberg_table(metadata_list)
 
-    # ias_no1_pivot 테이블만 데이터 적재
-    # copy_data(
-    #     metadata_list["h2521_ias_ias_no1_pivot"], start="20260201", end="20260301"
-    # )
-
-    df = query_data(metadata_list, departure="20260201", arrival="20260201")
-    df.write_csv("combined.csv")
-
-    # 테스트용
-    # df = copy_data_test(
-    #     metadata_list["h2521_ias_ias_no1_pivot"], start="20260201", end="20260202"
-    # )
-    # print(df)
-
-    # df = copy_data_test(
-    #     metadata_list["h2521_ias_ias_no2_pivot"], start="20260201", end="20260202"
-    # )
-    # print(df)
-
-    # df = copy_data_test(
-    #     metadata_list["h2521_vdr_vdvbw_pivot"], start="20260201", end="20260202"
-    # )
-    # print(df)
-
-    # df = copy_data_test(
-    #     metadata_list["h2521_vdr_sddpt_pivot"], start="20260201", end="20260202"
-    # )
-    # print(df)
-
-    # ias_no2_pivot 테이블 조인해서 데이터 적재
-    # copy_data_join(metadata_list)
+    insert_data2(col_names, metadata_list, start="20260101", end="20260301")
+    insert_data2(col_names, metadata_list, start="20260302", end="20260320")
 
 
 if __name__ == "__main__":
